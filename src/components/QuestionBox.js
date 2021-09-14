594e5d83f99267f9a3f5407364d10fd54a502dcf
ranjeetha-sn <ranjitha.sheel@gmail.com>
2021-09-14 17:03:22 +0530

    added env variable

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const QuestionBox = ({ question, options, selected }) => {
-  console.log("typing");
+  console.log("typing",process.env.REACT_APP_BACKEND_URL);
   const [answer, setAnswer] = useState(options);
   return (
     <div className="questionBox">
